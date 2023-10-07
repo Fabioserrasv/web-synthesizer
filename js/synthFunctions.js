f70be57b258af3a89bc226ae31c8d6f3691006fb
Fabioserrasv <fabioserra.sv@gmail.com>
2023-10-07 19:42:03 -0300

    First tests

---
+++ js/synthFunctions.js
@@ -0,0 +1,44 @@
+let oscArray = {}
+
+function playNote(event) {
+  if (event.buttons & 1) {
+    const note = event.target
+
+    if (!note.dataset["playing"]) {
+      const frequency = note.dataset["frequency"]
+      note.dataset["playing"] = true
+
+      const osc = actx.createOscillator();
+
+      osc.connect(actx.destination); // Connect osc with main audiocontext destination
+      osc.connect(gainNode); // Set volume control
+      osc.type = getCurrentWaveForm(); // Get wave type on Select
+
+      osc.frequency.setValueAtTime(frequency, actx.currentTime); // Set note frequency
+
+      osc.start();
+      oscArray[note.dataset["note"]] = osc
+    }
+  }
+}
+
+function stopNote(event) {
+  const note = event.target
+
+  if (note.dataset["playing"]) {
+    oscArray[note.dataset["note"]].stop();
+    delete oscArray[note.dataset["note"]];
+    delete note.dataset["playing"]
+  }
+}
+
+function getCurrentWaveForm() {
+  return document.getElementById("waveSelector").value
+}
+
+// VOLUME SECTION
+
+function changeVolume(event) {
+  volumeDisplay.innerHTML = event.target.value
+  gainNode.gain.value = volumeInput.value;
+}
